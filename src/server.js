84693df476b64c9ad2d5a8834d10bc38846e04af
Michael Zwierzanski <69387856+mz106@users.noreply.github.com>
2024-01-08 11:59:56 +0000

    enhancement: add user model

--- src/server.js
+++ src/server.js
@@ -3,14 +3,21 @@ const express = require("express");
 
 const port = process.env.PORT || 5001;
 
+const User = require("./user/model");
+
 const app = express();
 
 app.use(express.json());
+
+const syncTables = async () => {
+  await User.sync();
+};
 
 app.get("/health", (req, res) => {
   res.status(200).json({ message: "API is healthy" });
 });
 
 app.listen(port, () => {
+  syncTables();
   console.log(`App is listening on port ${port}`);
 });
---
+++ src/user/model.js
@@ -0,0 +1,27 @@
+// Create a user
+// username
+// email
+// password
+
+const { DataTypes } = require("sequelize");
+
+const sequelize = require("../db/connection");
+
+const User = sequelize.define("User", {
+  username: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
+
+module.exports = User;
