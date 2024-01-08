4a1c5ee0a1269a10911d0ba4ffd830e7426775c3
Michael Zwierzanski <69387856+mz106@users.noreply.github.com>
2024-01-08 15:11:31 +0000

    enhancement: add password hashing

--- src/user/routes.js
+++ src/user/routes.js
@@ -2,8 +2,9 @@ const { Router } = require("express");
 const userRouter = Router();
 
 const { addUser, getAllUsers } = require("./controllers");
+const { hashPass } = require("../middleware/auth");
 
-userRouter.post("/users", addUser);
+userRouter.post("/users", hashPass, addUser);
 userRouter.get("/users", getAllUsers);
 
 module.exports = userRouter;
